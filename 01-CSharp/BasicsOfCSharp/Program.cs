2cf00bb9aed9c5ace38410f5d07ab24bfd721800
unknown <kaurrevature@outlook.com>
2019-05-07 14:55:25 -0500

    Consturctors amd properties

--- 01-CSharp/BasicsOfCSharp/Program.cs
+++ 01-CSharp/BasicsOfCSharp/Program.cs
@@ -19,14 +19,21 @@ namespace BasicsOfCSharp
             name = Console.ReadLine();
             Console.WriteLine("Hello "+name); */
 
-            Employee emp=new Employee();
-            emp.firstName="Carol";
-            emp.lastName="Baxtor";
-            emp.ssn="2342Ahjghj";
-            emp.age=40;
-            emp.salary=10000.00M; 
-
-            emp.GetDetails();
+           // Employee emp=new Employee(); // default Constructor
+            // emp.firstName="Carol";
+            // emp.lastName="Baxtor";
+            // emp.ssn="2342Ahjghj";
+            // emp.age=40;
+            // emp.salary=10000.00M; 
+            // emp.ishuman=false; //cannot be  re assigned 
+            //emp.GetDetails();
+            /// Calling parametrized Constructor with name parameters
+            //Employee emp=new Employee(age:29,firstName:"Palmer",lastName:"Calgoris",ssn:"hjhjk45",salary:8500.00M); // parameterised Constructor
+           
+           // Calling parameterized Constructor with default values
+           Employee emp=new Employee("Felice","Sumargo","21321vhj");
+           emp._Salary=8563.23M;
+           Console.Write($"The Salary is {emp._Salary}");
         }
     }
 }
--- 01-CSharp/BasicsOfCSharp/Types.cs
+++ 01-CSharp/BasicsOfCSharp/Types.cs
@@ -3,15 +3,50 @@ using System;
 namespace BasicsOfCSharp{
 class Employee //Proper
  {
+    internal const bool ishuman=true;// Its value cannot be changed
      //variables - camelCase
+     //Declaration of variables
      internal string firstName, lastName, ssn;
      internal int age;
-     internal decimal salary;
+     private decimal salary;
 
+    //Properties: Smart fields to acccess the private variables outdide the class
+    public decimal _Salary{
+        get{
+            if(salary>5000)
+                return salary;
+            else 
+            return 0.00M;
+        }
+        //Read only property
+       /* set{
+                salary=value;
+        } */
+    }
      //Methods= Specification + definition (body)
      public void GetDetails(){
         // Console.WriteLine("Name {0} {1} has Social Security - {2}. The Salary is {3}.",firstName,lastName,ssn,salary);
-         
+         Console.WriteLine($"Name {firstName} {lastName} has Social Security - {ssn}. The Salary is {salary}.");
+     }
+
+     //Constructors in C# : Special methods which is used to initialize the memory to the objects
+     //parameter less Constructor
+     public Employee(){
+            firstName="Cameron";
+            lastName="Coley";
+            ssn="123hhjkjk";
+            age=30;
+            salary=8000.00M; 
+            Console.WriteLine($"Name {firstName} {lastName} has Social Security - {ssn}. The Salary is {salary}.");
+     }
+     //parameterized constructors
+     public Employee(string firstName,string lastName, string ssn, int age=18, decimal salary=5000.50M )
+     {
+         this.firstName=firstName;
+         this.lastName=lastName;
+         this.age=age;
+         this.ssn=ssn;
+         this.salary=salary;
          Console.WriteLine($"Name {firstName} {lastName} has Social Security - {ssn}. The Salary is {salary}.");
      }
 
