7273abc8c73aa61c930e8614e256a29b60b1c9ee
unknown <kaurrevature@outlook.com>
2019-05-07 10:58:41 -0500

    First CSharp program

---
+++ 01-CSharp/BasicsOfCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿//Namespace - logical collection of libraries
+//1. Predefined name space
+using System; 
+
+//2. User Defined namespace
+namespace BasicsOfCSharp
+{
+    //Types : Class, Structs, Enums, interfaces, Delegates
+    class Program
+    {
+        //Type Members: Methods, Variables, Constants, Properties
+        static string name= "";
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter your name ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello "+name);
+        }
+    }
+}
