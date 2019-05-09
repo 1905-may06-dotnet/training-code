c665ba05358872d42f5da63e5bcb96ef40c8b4e3
unknown <kaurrevature@outlook.com>
2019-05-09 16:24:24 -0500

    Exception Handling

---
+++ 01-CSharp/ExceptionsDemo/ExceptionsDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using NLog;
+
+namespace ExceptionsDemo
+{
+    class MyException:ApplicationException
+    {
+        public MyException(string message):base(message)
+        {
+            Console.WriteLine("my custom exception");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //vulnerable Line of code place in the Try block
+            try
+            {
+                TestException();
+            }
+            catch (FileNotFoundException e) {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+        static void TestException()
+        {
+            string path = @"C:\Revature\tst.txt";
+            StreamReader sr = null;
+            try
+            {
+                throw new MyException("Raised the user defined exception");
+                sr = new StreamReader(path);
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch (MyException)
+            {
+
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine($"Please check if the Directory exists!! - {ex.Message} ");
+            }
+            /*catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File name is incorrect !! ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }*/
+            finally
+            {
+                sr?.Close();
+            }
+        }
+    }
+}
