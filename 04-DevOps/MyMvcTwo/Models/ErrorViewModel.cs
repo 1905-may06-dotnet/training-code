94a03e33ed5c43e9286de871f3e70ba24a9ef1bd
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-06-07 15:46:09 -0500

    scaffold mymvctwo

---
+++ 04-DevOps/MyMvcTwo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyMvcTwo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
